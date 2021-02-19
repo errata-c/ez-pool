1a18637c9404bb91b93507f875448e42c25d95c7
errata-c <77643526+errata-c@users.noreply.github.com>
2021-02-19 10:21:46 -0500

    Improved package generation

--- postconfig.cmake
+++ postconfig.cmake
@@ -4,6 +4,6 @@ if(${PHMAP_HEADERS} STREQUAL "PHMAP_HEADERS-NOTFOUND")
 	message(FATAL_ERROR "Could not locate the headers for the parallel_hashmap library!")
 endif()
 
-set_property(TARGET ez-pool APPEND
+set_property(TARGET ez::pool APPEND
 	PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${PHMAP_HEADERS}"
 )
